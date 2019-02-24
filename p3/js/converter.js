24eb09ce7fd7db813c07523ad3d4595680a167b5
Kaan Tasbas <mktasbas@gmail.com>
2019-02-24 13:10:52 -0500

    Added and tested weight conversion

--- p3/js/converter.js
+++ p3/js/converter.js
@@ -5,3 +5,11 @@ function feetToMeters(valNum) {
 function metersToFeet(valNum) {
 	document.getElementById("outputFeet").innerHTML = (valNum * 3.281).toFixed(3);
 }
+
+function poundsToKilograms(valNum) {
+	document.getElementById("outputKilograms").innerHTML = (valNum / 2.205).toFixed(3);
+}
+
+function kilogramsToPounds(valNum) {
+	document.getElementById("outputPounds").innerHTML = (valNum * 2.205).toFixed(3);
+}
