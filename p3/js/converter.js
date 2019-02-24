ae43ec83631b2effd61fa834b0fefec197cfb74f
Kaan Tasbas <mktasbas@gmail.com>
2019-02-24 14:54:56 -0500

    Added comments to converter.js

--- p3/js/converter.js
+++ p3/js/converter.js
@@ -1,23 +1,34 @@
+// M. Kaan Tasbas | mktasbas@gmail.com
+// p3/converter.js
+
+// All conversion math was taken from Google's suggested formulas
+
+// Convert miles per hour to kilometers per hour
 function mphToKph(valNum) {
 	document.getElementById("outputKph").innerHTML = (valNum * 1.609).toFixed(3);
 }
 
+// Convert kilometers per hour to miles per hour
 function kphToMph(valNum) {
 	document.getElementById("outputMph").innerHTML = (valNum / 1.609).toFixed(3);
 }
 
+// Convert feet to meters
 function feetToMeters(valNum) {
 	document.getElementById("outputMeters").innerHTML = (valNum / 3.281).toFixed(3);
   }
 
+// Convert meters to feet
 function metersToFeet(valNum) {
 	document.getElementById("outputFeet").innerHTML = (valNum * 3.281).toFixed(3);
 }
 
+// Convert pounds to kilograms
 function poundsToKilograms(valNum) {
 	document.getElementById("outputKilograms").innerHTML = (valNum / 2.205).toFixed(3);
 }
 
+// Convert kilograms to pounds
 function kilogramsToPounds(valNum) {
 	document.getElementById("outputPounds").innerHTML = (valNum * 2.205).toFixed(3);
 }
