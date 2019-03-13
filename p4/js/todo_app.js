21b8fed381ae5e7a434c3495c2223cf675d380e1
Kaan Tasbas <mktasbas@gmail.com>
2019-03-12 22:51:49 -0400

    Add explanation comments to js functions

--- p4/js/todo_app.js
+++ p4/js/todo_app.js
@@ -1,44 +1,56 @@
+// Add an item to the list
 function addTodoItem() {
+	// Get text entry for item
 	var todoItem = $("#new_item").val();
-	$("#todo_list").append("<li><input type='checkbox'" + 
-						   " name='todo-item-done'" + 
-						   " class='todo-item-done'"+ 
-						   " value='" + todoItem + "' /> " + 
-						   todoItem +
-						   " <button class='delete_item'>"+
-						   "Delete</button></li>");
-	
-   $("#new_item").val("");
-  }
-  
-  function deleteTodoItem(e, item) {
+
+	// Add item to the unordered list
+	$("#todo_list").append("<li><input type='checkbox'" +
+		" name='todo-item-done'" +
+		" class='todo-item-done'" +
+		" value='" + todoItem + "' /> " +
+		todoItem +
+		" <button class='delete_item'>" +
+		"Delete</button></li>");
+
+	// Clear text entry in submission box
+	$("#new_item").val("");
+}
+
+// Delete an item from the list
+function deleteTodoItem(e, item) {
+	// Stop browser from acting on the click itself
 	e.preventDefault();
-	$(item).parent().fadeOut('slow', function() { 
-	  $(item).parent().remove();
+	// Remove item from list
+	$(item).parent().fadeOut('slow', function () {
+		$(item).parent().remove();
 	});
-  }
-  
-							 
-  function completeTodoItem() {  
+}
+
+// Mark item as complete
+function completeTodoItem() {
+	// Strike through item
 	$(this).parent().css("text-decoration", "line-through");
-  }
-  
-  
-  $(function() {
-   
-	 $("#add_item").on('click', function(e){
-	   e.preventDefault();
-	   addTodoItem()
-	 });
-	
-  //EVENT DELEGATION
-  //#todo-list is the event handler because .todo-item-delete doesn't exist when the document loads, it is generated later by a todo entry
-  //https://learn.jquery.com/events/event-delegation/
-	$("#todo_list").on('click', '.delete_item', function(e){
-	  var item = this; 
-	  deleteTodoItem(e, item)
+}
+
+// Event handlers
+$(function () {
+
+	// Add item button event handler
+	$("#add_item").on('click', function (e) {
+		// Stop browser from acting on click itself
+		e.preventDefault();
+		// Add item to list
+		addTodoItem()
+	});
+
+	// Delete item button event handler
+	// #todo_list used as handler because #delete_item doesn't exist until an entry is made
+	$("#todo_list").on('click', '.delete_item', function (e) {
+		var item = this;
+		deleteTodoItem(e, item)
 	})
-	
+
+	// Completed item checkbox event handler
 	$(document).on('click', ".item_done", completeTodoItem)
-  
-  });
+
+});
