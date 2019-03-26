fa3bdafbc68a4d67f4d58abdcf4b1a216979b675
Kaan Tasbas <mktasbas@gmail.com>
2019-03-25 22:33:58 -0400

    Update example code with new api. Tested working

--- p5/functions.php
+++ p5/functions.php
@@ -2,10 +2,11 @@
 function currencyConverter($from_Currency,$to_Currency,$amount) {	
 	$from_Currency = urlencode($from_Currency);
 	$to_Currency = urlencode($to_Currency);	
-	$get = file_get_contents("https://finance.google.com/finance/converter?a=1&from=$from_Currency&to=$to_Currency");
-	$get = explode("<span class=bld>",$get);
-	$get = explode("</span>",$get[1]);
-	$rate= preg_replace("/[^0-9\.]/", null, $get[0]);
+	error_log("Requesting conversion...");
+	$get = file_get_contents("https://free.currencyconverterapi.com/api/v6/convert?q={$from_Currency}_{$to_Currency}&compact=ultra&apiKey=9f6b065258edc8e4c3a5");
+	$get = explode(":",$get);
+	error_log("rate: " + $get[1]);
+	$rate = substr($get[1], 0, -1);
 	$converted_amount = $amount*$rate;
 	$data = array( 'rate' => $rate, 'converted_amount' =>$converted_amount, 'from_Currency' => strtoupper($from_Currency), 'to_Currency' => strtoupper($to_Currency));
 	echo json_encode( $data );	
